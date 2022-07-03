2fd79ea2a71fcfc297311ad005daeed6737b9105
Shane Cowen <dopefish89@gmail.com>
2022-07-03 00:12:14 -0700

    remove redundancies

--- scripts/script.js
+++ scripts/script.js
@@ -3,7 +3,6 @@ var computerScore = 0;
 const winCondition = 5;
 // Computer picks move randomly
 let computerPlay = () => {
-  // Chosses a random number between 1-3 to represent Rcok Paper and Scissors
   let randomChoice = Math.floor(Math.random() * 3);
 
   if(randomChoice == 0) {
@@ -28,7 +27,7 @@ let playRound = (playerSelection, computerSelection) => {
   let win =  "VICTORY!";
   let messageBox = document.querySelector('#results');
   
-  if(playerSelection == 'rock' || playerSelection == 1) {
+  if(playerSelection == 'rock') {
     if(computerSelection == 'r') {
       messageBox.textContent = tie + " You both played Rock!" + score(0, 0);
     } else if(computerSelection == 'p') {
@@ -36,7 +35,7 @@ let playRound = (playerSelection, computerSelection) => {
     } else { 
       messageBox.textContent = win + " Rock beats Scissors!" + score(1, 0);
     }
-  } else if(playerSelection == 'paper' || playerSelection == 2) {
+  } else if(playerSelection == 'paper') {
       if(computerSelection == 'r') {
         messageBox.textContent = win + " Paper beats Rock!" + score(1, 0);
       } else if(computerSelection == 'p') {
@@ -44,7 +43,7 @@ let playRound = (playerSelection, computerSelection) => {
       } else if(computerSelection == 's') {
         messageBox.textContent = lose + " Paper loses to Scissors!" + score(0, 1);
       }
-  } else if(playerSelection == 'scissors' || playerSelection == 3) {
+  } else if(playerSelection == 'scissors') {
       if(computerSelection == 'r') {
         messageBox.textContent = lose + " Rock beats Scissors!" + score(0, 1);
       } else if(computerSelection == 'p') {
@@ -53,14 +52,14 @@ let playRound = (playerSelection, computerSelection) => {
         messageBox.textContent = tie + " You both played Scissors!" + score(0, 0);
       }
   }
-  if(playerScore >= winCondition || computerScore >= winCondition) {
+  if(playerScore === winCondition || computerScore === winCondition) {
     const victoryMessage = () => messageBox.textContent = `\n\nCongrats! You win!\nFINAL SCORES: You: ${playerScore} \nComputer: ${computerScore}`;
     const loseMessage = () => messageBox.textContent = `\n\nYou LOST!\nFINAL SCORES: You: ${playerScore} \nComputer: ${computerScore}`;
     
-    if(playerScore >= winCondition)  {
+    if(playerScore === winCondition)  {
       victoryMessage();
     }
-    if(computerScore >= winCondition) {
+    if(computerScore === winCondition) {
       loseMessage();
     }
     playerScore = 0;
